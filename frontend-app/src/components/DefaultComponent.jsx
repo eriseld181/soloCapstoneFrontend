16c4da7a338b773ffd9ef09290b1d970f88c435b
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-10-08 16:24:29 +0200

    profile update

---
+++ frontend-app/src/components/DefaultComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Button, Card } from "react-bootstrap"
+import mainStyle from './Component.module.css'
+export default function DefaultComponent(props) {
+    return (
+        <Container>
+            <Card className={`${mainStyle.cardDesignClean} mt-5 mb-5 text-center`}>
+                <Card.Img className={`${mainStyle.bg} mx-auto ${mainStyle.imageContent} `} variant="top" src={props.img} />
+                <Card.Body className={`${mainStyle.bg} `}>
+                    <Card.Title className={`${mainStyle.title} mt-3 `}>{props.title}</Card.Title>
+                    <Card.Text className={`${mainStyle.textLabel} `}>
+                        {props.text}
+                    </Card.Text>
+                    <Button className={`mt-3 `} variant="primary">Post Now</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
--- frontend-app/src/components/MyTabs.jsx
+++ frontend-app/src/components/MyTabs.jsx
@@ -1,35 +1,40 @@
 import React from "react"
-import { Tab, Tabs, Container, Button, Card } from "react-bootstrap"
+import { Tab, Tabs, } from "react-bootstrap"
 import mainStyle from './Component.module.css'
+import DefaultComponent from "../components/DefaultComponent"
+
+
 export default function myTabs() {
     return (
 
         <Tabs variant="pills" transition={false} className={`justify-content-center`} defaultActiveKey="myPublications" id="uncontrolled-tab-example">
             <Tab className={`justify-content-center ${mainStyle.Tabs}`} eventKey="myPublications" title="Publications">
-                <Container style={{ outline: "solid red 1px" }}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-    </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-
-
-                </Container>
+                <DefaultComponent
+                    img="./publication.png"
+                    title="There is nothing to see now!"
+                    text="All your Publications will be shown here. Add a new post..."
+                />
             </Tab>
             <Tab className={`justify-content-center ${mainStyle.Tabs}`} eventKey="homeworks" title="Homeworks">
-                <Container>This is the place for Homeworks</Container>
+                <DefaultComponent
+                    img="./homeworks.png"
+                    title="There is nothing to see now!"
+                    text="All your Homeworks will be shown here. Add a new post..."
+                />
             </Tab>
             <Tab eventKey="notes" title="Notes">
-                <Container>  This is the place for personal notes</Container>
+                <DefaultComponent
+                    img="./notes.png"
+                    title="There is nothing to see now!"
+                    text="All your Notes will be shown here. Add a new post..."
+                />
             </Tab>
             <Tab eventKey="projects" title="Projects">
-                <Container>   This is the place for personal Projects</Container>
+                <DefaultComponent
+                    img="./projects.png"
+                    title="There is nothing to see now!"
+                    text="All your Publications will be shown here. Add a new post..."
+                />
             </Tab>
 
         </Tabs>
