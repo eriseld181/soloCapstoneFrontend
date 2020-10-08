3d72b33fd03af270c48342da63047dd726bad5b0
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-10-09 01:42:38 +0200

    adding new path

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import Login from "./pages/Login"
+import MainPage from './pages/MainPage'
 import style from "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
@@ -11,15 +12,10 @@ function App() {
       <Router>
 
         <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/home" exact component={Home} />
         </Switch>
       </Router >
 
